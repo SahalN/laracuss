3c336a85edca042108cedc921e247b9935b1cc1d
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2024-03-26 22:56:57 +0700

    Membuat UI Edit Profile Page

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,7 @@ Route::get('answers/1', function () {
 Route::get('users/sahaln', function () {
     return view('pages.users.show');
 })->name('users.show');
+
+Route::get('users/sahaln/edit', function () {
+    return view('pages.users.form');
+})->name('users.edit');
